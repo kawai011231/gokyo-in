bdaf7f82b995fcb6a695a288763c2cfc232aec88
kawai011231 <yousuke011231@gmail.com>
2022-10-14 13:06:44 +0900

    修正しました

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-// function handleClick() {
-//   result = nav.classList.contains("js-nav");
-//   if (result) {
-//     nav.classList.remove("js-nav");
-//     menuTrigger.classList.remove("js-closeTrigger");
-//   } else {
-//     nav.classList.add("js-nav");
-//     menuTrigger.classList.add("js-closeTrigger");
-//   }
-// }
-// const menuTrigger = document.getElementById("js-menuTrigger");
-// const nav = document.getElementById("js-nav");
-// menuTrigger.addEventListener("click", handleClick);
-
-var widget = document.getElementById("js-filter");
-var checkboxes = widget.querySelectorAll('.filter-cond input[type="checkbox"]');
-var checkedList = [];
-var filter = function () {
+const widget = document.getElementById("js-filter");
+const checkboxes = widget.querySelectorAll(
+  '.filter-cond input[type="checkbox"]'
+);
+const filter = () => {
   checkedList = [];
 
   Array.prototype.forEach.call(checkboxes, function (input) {
